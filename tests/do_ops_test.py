ef10dd330780727c42df660bd8ef6cd49bbff87a
#ZHANG CONG# <sun.jing@ntu.edu.sg>
2022-01-17 13:42:57 +0800

    rm test folder

--- tests/do_ops_test.py
+++
@@ -1,8 +0,0 @@
-from semiMDP.simulators import Simulator
-
-if __name__ == "__main__":
-    s = Simulator(5, 5)
-    g, r, done = s.observe()
-
-    for n in g.nodes:
-        print(n, g.nodes[n])
--- tests/idle_time reward and no first delayed op test
+++
@@ -1,6 +0,0 @@
-from pyjssp.simulators import Simulator
-
-if __name__ == "__main__":
-    s = Simulator(5, 5, delay=False)
-    print(s.get_doable_ops())
-    s.plot_graph()
